8e871eef43a665bf58db686e25f01829fdadf029
sopo9909 <hw.wh@takesolution.co.kr>
2023-11-20 13:30:41 +0900

    qwe

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-from fastapi import FastAPI, HTTPException
-import aioredis
+from fastapi import FastAPI, HTTPException, BackgroundTasks
+import redis
 import uvicorn
 
 app = FastAPI()
-redis = aioredis.from_url(
-    "redis://redisserver.dist-prd:6379", encoding="utf-8", decode_responses=True
+redis_client = redis.Redis(
+    host="redisserver.dist-prd", port=6379, decode_responses=True
 )
 
 
-@app.on_event("startup")
-async def startup_event():
-    await redis.initialize()
+def redis_set(key: str, value: str):
+    redis_client.set(key, value)
 
 
-@app.on_event("shutdown")
-async def shutdown_event():
-    await redis.close()
+def redis_delete(key: str):
+    redis_client.delete(key)
 
 
 @app.post("/create/{key}")
-async def create_key(key: str, value: str):
-    if await redis.exists(key):
+def create_key(background_tasks: BackgroundTasks, key: str, value: str):
+    if redis_client.exists(key):
         raise HTTPException(status_code=400, detail="Key already exists")
-    await redis.set(key, value)
+    background_tasks.add_task(redis_set, key, value)
     return {"key": key, "value": value}
 
 
 @app.delete("/delete/{key}")
-async def delete_key(key: str):
-    if not await redis.exists(key):
+def delete_key(background_tasks: BackgroundTasks, key: str):
+    if not redis_client.exists(key):
         raise HTTPException(status_code=404, detail="Key not found")
-    await redis.delete(key)
+    background_tasks.add_task(redis_delete, key)
     return {"detail": "Key deleted"}
 
 
 @app.get("/select/{key}")
-async def select_key(key: str):
-    value = await redis.get(key)
+def select_key(key: str):
+    value = redis_client.get(key)
     if value is None:
         raise HTTPException(status_code=404, detail="Key not found")
     return {"key": key, "value": value}

b80ebd2e79bdd40319e2541b65e3b62e3f1ff700
sopo9909 <hw.wh@takesolution.co.kr>
2023-11-21 10:25:31 +0900

    qweqwe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from rediscluster import RedisCluster
 import uvicorn
+import pika
+import uuid
 
 app = FastAPI()
 startup_nodes = [{"host": "redisserver.dist-prd", "port": "6379"}]  # Redis 클러스터 노드 정보
@@ -38,6 +40,40 @@ def select_key(key: str):
         raise HTTPException(status_code=404, detail="Key not found")
     return {"key": key, "value": value}
 
+
+rabbitmq_server = 'rabbitmq.common-prd'  # Change as needed
+queue_name = 'test_queue'
+
+# Establish RabbitMQ Connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_server))
+channel = connection.channel()
+channel.queue_declare(queue=queue_name)
+
+def insert_message(message):
+    channel.basic_publish(exchange='',
+                          routing_key=queue_name,
+                          body=message)
+    print(" [x] Sent %r" % message)
+
+def select_message():
+    method_frame, header_frame, body = channel.basic_get(queue=queue_name)
+    if method_frame:
+        print(" [x] Received %r" % body)
+        channel.basic_ack(method_frame.delivery_tag)
+        return body
+    else:
+        print("No message returned")
+        return None
+
+@app.post("/insert/")
+async def insert(message: str):
+    insert_message(message)
+    return {"message": "Message sent"}
+
+@app.get("/get/")
+async def select():
+    message = select_message()
+    return {"message": message}
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", access_log=True, port=8080)
